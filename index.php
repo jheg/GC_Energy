f6259d30269bd57b5f15e94e3dbfeb77796de1cf
MikeChipshop <mike@miniman-webdesign.co.uk>
2014-03-22 08:31:54 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Pathway+Gothic+One' rel='stylesheet' type='text/css'>	
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,700,300' rel='stylesheet' type='text/css'>
+  <meta charset="utf-8">
+
+  <title>GC Energy Ltd | The #1 Energy Recruiters</title>
+  <meta name="description" content="">
+  <meta name="author" content="Digimoose">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="css/grid.css">
+  <!-- jQuery. To be replaced by WP call at later date -->
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <!-- end jQuery -->
+  <script src="js/jquery.bxSlider.min.js"></script>
+  <script src="js/tabcontent.js" type="text/javascript"></script>
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+	<header>
+		<hgroup>
+		<nav class="clearfix">
+        	<div class="container clearfix">
+                <ul class="main-nav clearfix">
+                    <li id="current"><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Clients</a></li>
+                    <li><a href="#">Candidates</a></li>
+                    <li><a href="#">Jobs</a></li>
+                </ul>
+            </div>
+		</nav>
+		
+		</hgroup>
+	</header>
+	<section class="banner">
+    	<div class="slider">
+            <ul>
+                  <li><img src="img/banner-test-1.jpg" /></li>
+                  <li><img src="img/banner-test-2.jpg" /></li>
+            </ul>
+        </div>
+		<div class="container">
+        	<a href="#" class="main-logo">GC Energy Partners</a>
+        </div>
+	</section>
+	<div class="container"> 
+		<main class="grid_8 omega">
+			<section class="intro">
+				<h1>Headline/strapline to go here</h1>
+				<p>About us text. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+			<section class="tabbed_jobs">
+				<ul class="tabs" data-persist="true">
+				    <li><a href="#view1">Oil &amp; Gas</a></li>
+				    <li><a href="#view2">Renewable Energy </a></li>
+				    <li><a href="#view3">Mining</a></li>
+				    <li><a href="#view4">Finance</a></li>
+				</ul>
+				<div class="tabcontents">
+				    <div id="view1">
+				        <div class="job_excerpt hotjob">
+				        	<div class="grid_3 job_pic omega"><img src="" height="100" width="100"></div>
+				        	<div class="grid_6 desc_excerpt"><h2>MWD/LWD Engineer</h2><p>Our client is a leading oilfield service company operating worldwide. They strive by attracting and retaining top talent ...</p></div>
+				        	<div class="grid_3 omega sector"><a href="#" class="button">View Listing</a></div>
+				        </div>
+				        <div class="job_excerpt">
+				        	<div class="grid_3 job_pic omega"><img src="" height="100" width="100"></div>				        	
+				        	<div class="grid_6 desc_excerpt"><h2>MWD/LWD Engineer</h2><p>Our client is a leading oilfield service company operating worldwide. They strive by attracting and retaining top talent ...</p></div>
+				        	<div class="grid_3 omega sector"><a href="#" class="button">View Listing</a></div>
+				        </div>
+				        <div class="job_excerpt">
+				        	<div class="grid_3 job_pic omega"><img src="" height="100" width="100"></div>
+				        	<div class="grid_6 desc_excerpt"><h2>MWD/LWD Engineer</h2><p>Our client is a leading oilfield service company operating worldwide. They strive by attracting and retaining top talent ...</p></div>
+				        	<div class="grid_3 omega sector"><a href="#" class="button">View Listing</a></div>
+				        </div>
+                        <div class="view-more clearfix">
+                        	<a href="#">View more in Gas &amp; Oil &raquo;</a>
+                        </div>
+				    </div>				    
+				    <div id="view2">
+				        content 2
+				    </div>
+				    <div id="view3">
+				        content 3
+				    </div>
+				    <div id="view4">
+				    	content 4
+				    </div>
+				</div>
+			</section>
+		</main>
+		<section class="sidebar grid_4 omega">
+			<section class="frame">
+				<h2>BROWSE JOBS BY INDUSTRY</h2>
+				<ul class="cats">
+					<li><a href="#">OIL &amp; GAS</a></li>
+					<li><a href="#">RENEWABLE ENERGY</a></li>
+					<li><a href="#">MINING</a></li>
+					<li><a href="#">FINANCE</a></li>
+				</ul>
+			</section>			
+			<section id="send_cv" class="frame">
+				<h2>Send us your CV</h2>
+				<p>Some text that appeals to potential candidates and encourages them to send you their cv.</p>
+				<form action="/my-handling-form-page" method="post">
+					<div>
+						<label for="first_name"></label>
+						<input type="text" id="first_name" tabindex="1" placeholder="first name" />
+					</div>
+					<div>
+						<label for="last_name"></label>
+						<input type="text" id="last_name" tabindex="2" placeholder="last name" />
+					</div>
+					<div>
+						<label for="email"></label>
+						<input type="email" id="email" tabindex="3" placeholder="email" />
+					</div>
+					<div>
+						<label for="tel"></label>
+						<input type="tel" id="tel" tabindex="4" placeholder="tel" />
+					</div>
+					<div>
+						<label for="msg"></label>
+						<textarea id="msg" tabindex="5" placeholder="OPTIONAL: enter a message for example: I'm interested in job ID00500"></textarea>
+					</div>
+					<div>
+						<label for="file"></label>
+						<input  id="file" type="file" tabindex="6" value="Upload CV">
+					</div>
+					<div>
+						<input id="submit" tabindex="7" value="Send" type="submit">
+					</div>		
+				</form>
+			</section>
+			
+		</section>
+	</div>
+	<div class="clearfix"></div>
+	<footer>
+		<div class="container">
+			<section class="testimonials grid_3">
+				<h2>RECENT TESTIMONIALS</h2>
+				<div class="testimonial">
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at blandit arcu. Quisque vel eros.</p>
+					<p class="name">Jason Hegarty</p>
+					<p class="locartion">England</p>
+				</div>
+			</section>
+			<section class="address grid_3">
+				<h2>OUR ADDRESS</h2>
+				<P>GC Energy Ltd<br />Street Address<br />City<br />Post Code</P>
+			</section>
+			<section class="sectors grid_3">
+				<h2>BROWSE JOBS BY SECTOR</h2>
+				<ul>
+					<li><a href="#">OIL &amp; GAS</a></li>
+					<li><a href="#">RENEWABLE ENERGY</a></li>
+					<li><a href="#">MINING</a></li>
+					<li><a href="#">FINANCE</a></li>
+				</ul>
+			</section>
+			<section class="grid_3 omega">
+			<h2>GET IN TOUCH</h2>
+			<p>Fill out the form and submit your message</p>
+			<form action="/my-handling-form-page" method="post">
+				<div>
+					<label for="first_name"></label>
+					<input type="text" id="first_name" tabindex="1" placeholder="first name" />
+				</div>
+				<div>
+					<label for="last_name"></label>
+					<input type="text" id="last_name" tabindex="2" placeholder="last name" />
+				</div>
+				<div>
+					<label for="email"></label>
+					<input type="email" id="email" tabindex="3" placeholder="email" />
+				</div>
+				<div>
+					<label for="tel"></label>
+					<input type="tel" id="tel" tabindex="4" placeholder="tel" />
+				</div>
+				<div>
+					<label for="msg"></label>
+					<textarea id="msg" tabindex="5" placeholder="OPTIONAL: enter a message for example: I'm interested in job ID00500"></textarea>
+				</div>
+				<div>
+					<input id="submit" tabindex="6" value="Send" type="submit">
+				</div>		
+	    	</form>
+			</section>
+		</div>
+	</footer>
+        <script type="text/javascript">
+
+	$(document).ready(function(){
+		
+		$('.slider ul').bxSlider({
+			pager		:	true,
+			auto		:	true,
+			controls	:	true,
+		});
+		 
+	});
+</script>
+</body>
+</html>
---
+++ sectors.php
@@ -0,0 +1,39 @@
+<html>
+<body>
+<h1>Sectors</h1>
+
+<h2>Oil &amp; Gas</h2>
+<ul>
+	<li>Well Services</li>
+	<li>Drilling</li>
+	<li>Subsea Technical<ul>
+		<li>ROV</li>
+		<li>Survey &amp; Inspection</li>
+	</ul></li>
+	<li>Subsea Engineering<ul>
+		<li>Pipeline</li>
+		<li>Structural</li>
+	</ul></li>
+</ul>
+<h2>Renewable Energy</h2>
+<ul>
+	<li>Wind</li>
+	<li>Wave &amp; Tidal</li>
+	<li>Solar</li>
+	<li>Waste</li>
+</ul>
+<h2>Mining</h2>
+<ul>
+	<li></li>
+</ul>
+<h2>Finance</h2>
+<ul>
+<li>Commodities</li>
+<li>Funds &amp; Investment</li>
+</ul>
+
+Need to understand Sectors, Sub_Sectors &amp; Jobs.
+
+
+</body>
+</html>
