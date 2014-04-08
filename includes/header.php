6a324169529691e29e9ec392fdb4dfe329891be2
jheg <emailheg@gmail.com>
2014-04-08 23:56:02 +0100

    responive nav
    might need to tweak js for wordpress: if so visit this link and see
    comments:
    http://themetaq.com/articles/responsive-toggle-menu

--- includes/header.php
+++ includes/header.php
@@ -8,8 +8,10 @@
         	<div class="grid_6 none">
 
         	<p class="tel">Call our team today <a href="#">0207 183 9509</a></p>
+
 			<nav class="grid_12 clearfix">
 	        	<div class="clearfix">
+	        		<h2 class="navheader slide_trigger">Menu <span></span></h2>
 	                <ul class="main-nav clearfix">
 	                    <li id="current"><a href="#">Home</a></li>
 	                    <li><a href="about.php">About</a></li>
@@ -21,5 +23,26 @@
 			</nav>
 			</div>
 		</div>
-		
+		<script type="text/javascript">
+		$(document).ready(function(){
+    $('.slide_trigger').collapsable();
+});
+		/* MOBILE COLLAPSE MENU */
+		(function($) {
+		  $.fn.collapsable = function(options) {
+		    // iterate and reformat each matched element
+		    return this.each(function() {
+		      // cache this:
+		      var obj = $(this);
+		      var tree = obj.next('.main-nav');
+		      obj.click(function(){
+		        if( obj.is(':visible') ){tree.toggle();}
+		      });
+		      $(window).resize(function(){
+		        if ( $(window).width() <= 570 ){tree.attr('style','');};
+		      });
+		    });
+		  };
+		})(jQuery);
+		</script>
 	</header>
